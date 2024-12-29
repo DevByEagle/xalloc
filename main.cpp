a9c1cdf15cf9c6b8b99e0cd834961b44ec845b16
Aaron Thompson <athompsonbriley.56@gmail.com>
2024-12-28 22:56:32 -0500

    Updated Code

--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 #include "xalloc.h"
 
 int main() {
-    int* a = (int*) xalloc(sizeof(int));
-    if (a) {
-        std::cout << "Allocated memory for 'a', value: " << a << std::endl;
-    } else {
-        std::cerr << "Memory allocation failed for 'a'" << std::endl; 
-        printf("Memory allocation failed for 'a'\n");
-    }
+    int* a = (int*)xalloc(sizeof(int));
+    int* b = (int*)xalloc(sizeof(int));
 
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
 }
