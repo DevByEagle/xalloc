6b44089ad1eb4e6a7b9ba62bb60fab57ca5681cd
Aaron Thompson <athompsonbriley.56@gmail.com>
2024-12-28 22:44:06 -0500

    Add xalloc usage for integer allocation in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 
 int main() {
     void* ptr1 = xalloc(26);
+    void* ptr2 = xalloc(sizeof(int));
 
     std::cout << ptr1 << std::endl;
+    std::cout << ptr2 << std::endl;
 
     xfree(ptr1);
+    xfree(ptr2);
 
     return 0;
 }
